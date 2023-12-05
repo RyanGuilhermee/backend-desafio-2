a9395e582d68f61401b3a828f4cf72f3a05bc8d6
Ryan Guilherme <ryan.guilherme.740@gmail.com>
2023-12-05 13:57:29 -0300

    update fields in schema

---
+++ prisma/migrations/20231205150152_update_fieds/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "usuarios" ADD COLUMN "data_atualizacao" DATETIME;
+ALTER TABLE "usuarios" ADD COLUMN "data_criacao" DATETIME;
+ALTER TABLE "usuarios" ADD COLUMN "ultimo_login" DATETIME;
