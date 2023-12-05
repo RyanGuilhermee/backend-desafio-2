3c1f7aeee4ef3bf3f9282d6476ea9ae7e296ca21
Ryan Guilherme <ryan.guilherme.740@gmail.com>
2023-12-05 13:59:16 -0300

    add find user dto

--- src/users/dto/find-user.dto.ts
+++ src/users/dto/find-user.dto.ts
@@ -1,21 +1,10 @@
-import { IsNotEmpty } from 'class-validator';
-
-export class CreateUserDto {
-  @IsNotEmpty()
+export class FindUserDto {
+  id: string;
   nome: string;
-
-  @IsNotEmpty()
   email: string;
-
-  @IsNotEmpty()
   senha: string;
-
-  @IsNotEmpty()
   telefones: [{ numero: string; ddd: string }];
-
   data_criacao: Date;
-
   data_atualizacao: Date;
-
   ultimo_login: Date;
 }
