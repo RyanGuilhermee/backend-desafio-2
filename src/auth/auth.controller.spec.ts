97c5599055482739405d466b710d240929c56e50
Ryan Guilherme <ryan.guilherme.740@gmail.com>
2023-12-06 14:55:40 -0300

    add unit tests to auth resource

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,20 +1,54 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
+import { DeepMocked, createMock } from '@golevelup/ts-jest';
+import { SignInDto } from './dto/signin.dto';
+import { FindUserDto } from '../users/dto/find-user.dto';
+import { CreateUserDto } from '../users/dto/create-user.dto';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let authService: DeepMocked<AuthService>;
+  let usersService: DeepMocked<UsersService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
-    }).compile();
+    })
+      .useMocker(createMock)
+      .compile();
 
     controller = module.get<AuthController>(AuthController);
+    authService = module.get(AuthService);
+    usersService = module.get(UsersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('signIn', () => {
+    it('should sign in an user successfully', async () => {
+      const findUserDto = new FindUserDto();
+
+      authService.signIn.mockResolvedValue(findUserDto);
+
+      await expect(controller.signIn(new SignInDto())).resolves.toEqual(
+        findUserDto,
+      );
+    });
+  });
+
+  describe('signUp', () => {
+    it('should sign up an user successfully', async () => {
+      const findUserDto = new FindUserDto();
+
+      usersService.create.mockResolvedValue(findUserDto);
+
+      await expect(controller.signUp(new CreateUserDto())).resolves.toEqual(
+        findUserDto,
+      );
+    });
   });
 });
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,67 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
+import { DeepMocked, createMock } from '@golevelup/ts-jest';
+import * as bcrypt from 'bcrypt';
+import { FindUserDto } from '../users/dto/find-user.dto';
+import { UnauthorizedException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let usersService: DeepMocked<UsersService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [AuthService],
-    }).compile();
+    })
+      .useMocker(createMock)
+      .compile();
 
     service = module.get<AuthService>(AuthService);
+    usersService = module.get(UsersService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('signIn', () => {
+    it('should sign in an user successfully', async () => {
+      const findUserDto = new FindUserDto();
+
+      usersService.findOneByEmail.mockResolvedValueOnce(findUserDto);
+
+      jest
+        .spyOn(bcrypt, 'compare')
+        .mockImplementation(() => Promise.resolve(true));
+
+      await expect(service.signIn('test@email', 'senha')).resolves.toEqual(
+        findUserDto,
+      );
+    });
+
+    it('should throw an exception if user email is wrong', async () => {
+      usersService.findOneByEmail.mockResolvedValueOnce(null);
+
+      jest
+        .spyOn(bcrypt, 'compare')
+        .mockImplementation(() => Promise.resolve(false));
+
+      await expect(
+        service.signIn('test2@email', 'senha'),
+      ).rejects.toBeInstanceOf(UnauthorizedException);
+    });
+
+    it('should throw an exception if user password is wrong', async () => {
+      usersService.findOneByEmail.mockResolvedValueOnce(new FindUserDto());
+
+      jest
+        .spyOn(bcrypt, 'compare')
+        .mockImplementation(() => Promise.resolve(false));
+
+      await expect(service.signIn('test@email', '')).rejects.toBeInstanceOf(
+        UnauthorizedException,
+      );
+    });
   });
 });
